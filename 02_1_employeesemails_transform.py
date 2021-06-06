55cb39f7654ff75af49f6392de58d15c25d7574c
FedeScenna <federico.scenna@gmail.com>
2021-06-06 15:10:17 -0300

    convierte ipynb a py

---
+++ 02_1_employeesemails_transform.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+
+df = pd.read_csv("data/email headers.csv", sep=",", encoding="cp1252")
+
+long_df=pd.DataFrame(data = {"from": [],
+                            "to":[],
+                            "date":[],
+                            "subject":[]})
+
+for f in range(0,len(df["From"])):
+    # ver a cuantos les mando
+    to_array = df["To"][f].split(", ")
+    # y replicar el resto de los datos
+    for t in to_array:
+        single_df = pd.DataFrame(data = {"from": df["From"][f],
+            "to": t,
+            "date": df["Date"][f],
+            "subject":df["Subject"][f]
+        },index = [0])
+        long_df=long_df.append(single_df, ignore_index = True)
+
+long_df.to_csv("data/employee_headers_long.csv", index = False)
+
+
